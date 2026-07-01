6861183f4d9e6be802af1de527afd792432121e0
xYukiLove <xyukilovely@proton.me>
2026-07-01 11:48:55 -0400

    Refactor configuration.nix and update udev rules
    Refactor configuration.nix to improve organization and clarity. Update udev rules and enable additional services.

--- configuration.nix
+++ configuration.nix
@@ -1,38 +1,28 @@
 { config, pkgs, inputs, pkgs-stable,... }:
-
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
       ./noctalia.nix
     ];
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   fileSystems."/mnt/linuxgames" = {
     device = "UUID=187929c8-4308-4dae-9c0d-c25c3680f8c6";
     fsType = "btrfs";
-    options = [ "compress=zstd" "noatime" ]; # optional but common for btrfs
+    options = [ "compress=zstd" "noatime" ];   
   };
   services.xserver.videoDrivers = [ "amdgpu" ];
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
   #pkgs.cachyosKernels.linuxPackages-cachyos-latest-zen4; #pkgs.linuxPackages_latest;
-  networking.hostName = "nixos-btw"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos-btw";
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = false;
   };
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  # Enable networking
   networking.networkmanager.enable = true;
-  # Set your time zone.
   time.timeZone = "America/New_York";
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
@@ -46,82 +36,41 @@
     LC_TIME = "en_US.UTF-8";
   };
   services.udev.extraRules = ''
-  # WLmouse Sword X
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a879", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a879", MODE="0660", GROUP="input"
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a878", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a878", MODE="0660", GROUP="input"
-
-  # WLmouse Beast X Mini Pro
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a868", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a868", MODE="0660", GROUP="input"
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a869", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a869", MODE="0660", GROUP="input"
-
-  # WLmouse Beast X
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a883", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a883", MODE="0660", GROUP="input"
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a884", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a884", MODE="0660", GROUP="input"
-
-  # EGG OP18K
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3367", ATTRS{idProduct}=="1964", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="3367", ATTRS{idProduct}=="1964", MODE="0660", GROUP="input"
-
-  # G-Wolves HSK Pro 4k
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="33e4", ATTRS{idProduct}=="5807", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="33e4", ATTRS{idProduct}=="5807", MODE="0660", GROUP="input"
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="33e4", ATTRS{idProduct}=="5808", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="33e4", ATTRS{idProduct}=="5808", MODE="0660", GROUP="input"
-
-  # Corne V4
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="4653", ATTRS{idProduct}=="0004", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="4653", ATTRS{idProduct}=="0004", MODE="0660", GROUP="input"
-
-  # Vault 35 WKL Pipboy
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="a457", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="a457", MODE="0660", GROUP="input"
-
-  # Wooting 60HE+
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="31e3", ATTRS{idProduct}=="1322", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="31e3", ATTRS{idProduct}=="1322", MODE="0660", GROUP="input"
-
-  # Finalmouse ULX Sakura
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="361d", ATTRS{idProduct}=="0100", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="361d", ATTRS{idProduct}=="0100", MODE="0660", GROUP="input"
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="361d", ATTRS{idProduct}=="0102", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="361d", ATTRS{idProduct}=="0102", MODE="0660", GROUP="input"
-
-  # MCHOSE L7 Ultra
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="5253", ATTRS{idProduct}=="1020", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="5253", ATTRS{idProduct}=="1020", MODE="0660", GROUP="input"
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="5253", ATTRS{idProduct}=="00b1", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="5253", ATTRS{idProduct}=="00b1", MODE="0660", GROUP="input"
-
-  # WLmouse Huan
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a863", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a863", MODE="0660", GROUP="input"
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a864", MODE="0660", GROUP="input"
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a864", MODE="0660", GROUP="input"
-'';
-  # Enable udev rules
+    # EGG OP18K
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3367", ATTRS{idProduct}=="1964", MODE="0660", GROUP="input"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="3367", ATTRS{idProduct}=="1964", MODE="0660", GROUP="input"
+    # Corne V4
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="4653", ATTRS{idProduct}=="0004", MODE="0660", GROUP="input"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="4653", ATTRS{idProduct}=="0004", MODE="0660", GROUP="input"
+    # Vault 35 WKL Pipboy
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="a457", MODE="0660", GROUP="input"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="a457", MODE="0660", GROUP="input"
+    # MCHOSE L7 Ultra
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="5253", ATTRS{idProduct}=="1020", MODE="0660", GROUP="input"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="5253", ATTRS{idProduct}=="1020", MODE="0660", GROUP="input"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="5253", ATTRS{idProduct}=="00b1", MODE="0660", GROUP="input"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="5253", ATTRS{idProduct}=="00b1", MODE="0660", GROUP="input"
+    # WLmouse Huan
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a863", MODE="0660", GROUP="input"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a863", MODE="0660", GROUP="input"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a864", MODE="0660", GROUP="input"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a864", MODE="0660", GROUP="input"
+    # WLmouse Beast G Mini
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a860", MODE="0660", GROUP="input"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a860", MODE="0660", GROUP="input"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a861", MODE="0660", GROUP="input"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a861", MODE="0660", GROUP="input"
+  '';
   services.udev.enable = true;
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
-  services.displayManager.sddm.wayland = {
+  services.displayManager.sddm = {
     enable = true;
   };
-  #services.desktopManager.gnome.enable = true;
-  #services.desktopManager.plasma6.enable = true;
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-  # Enable CUPS to print documents.
   services.printing.enable = true;
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   security.polkit.enable = true;
@@ -131,41 +80,46 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  systemd.user.settings.Manager = {
+    DefaultEnvironment = [
+      "XDG_SESSION_TYPE=wayland"
+      "XDG_CURRENT_DESKTOP=mango"
+    ];
+  };
   xdg.portal = {
     enable = true;
-    xdgOpenUsePortal = true;
     extraPortals = [
+      pkgs.xdg-desktop-portal-wlr
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-gnome
-      pkgs.xdg-desktop-portal-wlr
     ];
     config = {
-      common.default = [ "gnome" ];
+      common = {
+        default = [ "gtk" "wlr" "gnome" ];
+	"org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+        "org.freedesktop.impl.portal.Inhibit" = [ "none" ];
+      };
+      wlroots = {
+      };
     };
   };
   nix.settings = {
     extra-substituters = [
-    "https://noctalia.cachix.org"
-    "https://nix-gaming.cachix.org"
-    "https://prismlauncher.cachix.org"
-    "https://yazi.cachix.org"
+      "https://noctalia.cachix.org"
+      "https://nix-gaming.cachix.org"
+      "https://prismlauncher.cachix.org"
+      "https://yazi.cachix.org"
     ];
     extra-trusted-public-keys = [
-    "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-    "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-    "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
-    "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
+      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
     ];
   };
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.allie = {
     isNormalUser = true;
     description = "Allie";
@@ -180,8 +134,8 @@
   programs.zsh.enable = true;
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    remotePlay.openFirewall = true; 
+    dedicatedServer.openFirewall = true;
   };
   programs.spicetify =
   let
@@ -197,22 +151,34 @@
     theme = spicePkgs.themes.starryNight;
     colorScheme = "Base";
   };
-  programs.neovim = {
+  programs.nixvim = {
     enable = true;
-    configure = {
-      customRC = ''
-        set number
-	set list
-	set termguicolors
-      '';
+    colorschemes.kanagawa-paper.enable = true;
+    globals.mapleader = " ";
+    opts = {
+      number = true;
+      shiftwidth = 2;
+    };
+    plugins.colorizer = {
+      enable = true;
+      settings = {
+        user_default_options = {
+      	  css = true;
+	  css_fn = true;
+	  rgb = true;
+	  hsl = true;
+	  names = true;
+	  tailwind = true;
+	  mode = "background";
+	};
       };
+    };
   };
   programs.gamemode.enable = true;
   environment.variables = {
     XCURSOR_THEME = "Shinobu-Oshino";
     XCURSOR_SIZE = "24";
   };
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     polkit_gnome
@@ -224,7 +190,6 @@
     zoxide
     bat
     wget
-    vesktop
     git
     kitty
     alacritty
@@ -264,10 +229,16 @@
     slurp
     swaybg
     waybar
-    flameshot
     cava
     wlogout
-    discord-canary
+    vscode
+    obsidian
+    lutris
+    gamescope
+    discord
+    pavucontrol
+    kdePackages.kdenlive
+    dunst
     (pkgs.wrapOBS {
       plugins = with pkgs.obs-studio-plugins; [
         wlrobs
@@ -283,27 +254,7 @@
     noto-fonts-cjk-serif
     noto-fonts-color-emoji
   ];
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-  # List services that you want to enable:
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-  # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 53317 ];
   networking.firewall.allowedUDPPorts = [ 53317 ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.11"; # Did you read the comment?
-
+  system.stateVersion = "26.11";
 }
