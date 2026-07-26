d9bb6922d512751ddf5810b1eb526aec56939269
xYukiLove <xyukilovely@proton.me>
2026-07-26 19:58:14 -0400

    Create xdg2.nix for XDG configuration
    without wlr

---
+++ nixosModules/xdg2.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+{
+  xdg.portal = {
+    enable = true;
+  };
+  xdg.mime = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+      "x-scheme-handler/about" = "librewolf.desktop";
+      "x-scheme-handler/unknown" = "librewolf.desktop";
+    }; 
+  };
+}
+
