934f5dac490ac30101bcc5185b442a2818d37226
xYukiLove <xyukilovely@proton.me>
2026-07-26 20:06:49 -0400

    Add spicetify module with default configuration

---
+++ nixosModules/spicetify.nix
@@ -0,0 +1,17 @@
+{ pkgs, inputs, ... }:
+{
+  programs.spicetify =
+  let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  in
+  {
+    enable = true;
+    enabledExtensions = with spicePkgs.extensions; [
+      adblockify
+      hidePodcasts
+      shuffle
+    ];
+    theme = spicePkgs.themes.starryNight;
+    colorScheme = "Base";
+  };
+}
