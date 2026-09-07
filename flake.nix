ca8b0a250bf40d6c086a1dbe5a2f2c736cd2598e
xYukiLove <xyukilovely@proton.me>
2026-09-07 17:53:16 -0400

    Removed lanzaboote and added hyprland.

--- flake.nix
+++ flake.nix
@@ -7,20 +7,12 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel";
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.1.0";
-      inputs.nixpkgs.follows = "nixpkgs";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     prismlauncher = {
       url = "github:PrismLauncher/PrismLauncher";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    yazi = {
-      url = "github:sxyazi/yazi";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    mangowm = {
-      url = "github:mangowm/mango";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     helium = {
@@ -36,63 +28,50 @@
     self,
     nixpkgs,
     nixpkgs-unstable,
-    lanzaboote,
     spicetify-nix,
     prismlauncher,
-    yazi,
-    mangowm,
     nixvim,
     helium,
     nix-flatpak,
     noctalia,
     nix-cachyos-kernel,
+    hyprland,
     ...
   }: 
   {
     nixosConfigurations = {
       nixos-btw = nixpkgs.lib.nixosSystem {
         specialArgs = 
-	let
+	    let
           system = "x86_64-linux";
-	in {
+	    in {
           inherit inputs;
         };
         modules = [
           ./configuration.nix
-	  ./nixosModules/spicetify.nix
-	  ./nixosModules/nixvim.nix
-	  ./nixosModules/mangowm.nix
-	  inputs.mangowm.nixosModules.mango
-	  spicetify-nix.nixosModules.spicetify
-	  nixvim.nixosModules.nixvim
-	  lanzaboote.nixosModules.lanzaboote
-	  nix-flatpak.nixosModules.nix-flatpak
-	  ({ pkgs, lib, ... }:
-	  {
-	    nixpkgs.overlays = [
-	      (final: prev: {
-	        unstable = import inputs.nixpkgs-unstable {
-		  system = final.stdenv.hostPlatform.system;
-		  config.allowUnfree = true;
-		};
-	      })
-	      nix-cachyos-kernel.overlays.default
-	    ];
-	    environment.systemPackages = [
-	      pkgs.sbctl
-	      inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-	      prismlauncher.packages.${pkgs.stdenv.hostPlatform.system}.prismlauncher
-	      inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.default
-	      (yazi.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
-	        _7zz = pkgs._7zz-rar;
-	      })
+	      ./nixosModules/spicetify.nix
+	      ./nixosModules/nixvim.nix
+	      ./nixosModules/hyprland.nix
+	      spicetify-nix.nixosModules.spicetify
+	      nixvim.nixosModules.nixvim
+	      nix-flatpak.nixosModules.nix-flatpak
+	      ({ pkgs, lib, ... }:
+	      {
+	        nixpkgs.overlays = [
+	          (final: prev: {
+	            unstable = import inputs.nixpkgs-unstable {
+		          system = final.stdenv.hostPlatform.system;
+		          config.allowUnfree = true;
+		        };
+	          })
+	          nix-cachyos-kernel.overlays.default
+	        ];
+	        environment.systemPackages = [
+	          inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+	          prismlauncher.packages.${pkgs.stdenv.hostPlatform.system}.prismlauncher
+	          inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.default
             ];
-	    boot.loader.systemd-boot.enable = lib.mkForce false;
-	    boot.lanzaboote = {
-	      enable = true;
-	      pkiBundle = "/var/lib/sbctl";
-	    };
-	  })
+	      })
         ];
       };
     };
