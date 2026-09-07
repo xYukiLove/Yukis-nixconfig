fd64f897c0fe6f29bf309b73f7bacdfc79f91225
xYukiLove <xyukilovely@proton.me>
2026-09-07 17:50:43 -0400

    Modify NixOS configuration for boot and user settings
    Updated boot loader configuration and user settings.

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,29 @@
-{ config, pkgs, inputs, pkgs-stable, lib, ... }:
+{ config, pkgs, ... }:
 {
   imports =
     [ 
       ./hardware-configuration.nix
       ./nixosModules/nixvim.nix
-      ./nixosModules/peripherials.nix
-      ./nixosModules/xdg.nix
       ./nixosModules/spicetify.nix
-      ./nixosModules/mangowm.nix
+      ./nixosModules/hyprland.nix
+      ./nixosModules/peripherials.nix
+      ./nixosModules/zsh.nix
     ];
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.limine.enable = true;
+  boot.loader.limine.secureBoot.enable = true;
+  boot.loader.limine.extraEntries = ''
+    /Windows
+      protocol: efi
+      path: uuid(47e51e77-37c9-4049-834e-2b1cc323d2ba):/EFI/Microsoft/Boot/bootmgfw.efi
+  '';
   boot.loader.efi.canTouchEfiVariables = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  boot.kernelPackages = pkgs.unstable.linuxPackages_latest;
   fileSystems."/mnt/linuxgames" = {
     device = "UUID=84865633-a9a6-41c5-9998-870de2e1549d";
     fsType = "ext4";
   };
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  boot.kernelPackages = pkgs.unstable.linuxPackages_latest;
-  #boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-zen4;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   networking.hostName = "nixos-btw";
-  users.users.yuki = {
-    isNormalUser = true;
-    description = "Yuki";
-    shell = pkgs.zsh;
-    extraGroups = [ "networkmanager" "wheel" "input" ];
-    packages = with pkgs; [
-      kdePackages.ark
-      kdePackages.kdenlive
-    ];
-  };
   services.blueman.enable = true;
   hardware.bluetooth = {
     enable = true;
@@ -54,40 +48,31 @@
     LC_TIME = "en_US.UTF-8";
   };
   services.xserver.enable = true;
-  services.xserver.displayManager.startx.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
+  services.printing.enable = true;
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  security.polkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    wireplumber.enable = true;
   };
-  nix.settings = {
-    extra-substituters = [
-      "https://nix-gaming.cachix.org"
-      "https://prismlauncher.cachix.org"
-      "https://yazi.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
-      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+  users.users."luna" = {
+    isNormalUser = true;
+    description = "luna";
+    extraGroups = [ "networkmanager" "wheel" "input" ];
+    packages = with pkgs; [
+      kdePackages.kdenlive
+      krita
+      emojipick
     ];
   };
-  programs.zsh.enable = true;
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; 
-    dedicatedServer.openFirewall = true;
-  };
-  programs.gamemode.enable = true;
   environment.variables = {
     XCURSOR_THEME = "Shinobu-Oshino";
     XCURSOR_SIZE = "32";
@@ -96,13 +81,30 @@
     enable = true;
     packages = [
       "org.vinegarhq.Sober"
-      "com.boxy_svg.BoxySVG"
       "com.discordapp.Discord"
     ];
   };
+  nix.settings = {
+    extra-substituters = [
+      "https://nix-gaming.cachix.org"
+      "https://prismlauncher.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
+    ];
+  };
+  programs.firefox.enable = true;
   nixpkgs.config.allowUnfree = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; 
+    dedicatedServer.openFirewall = true;
+  };
   environment.systemPackages = with pkgs; [
+    adw-gtk3
     ani-cli
+    vim
     bat
     blueman
     bolt-launcher
@@ -113,21 +115,20 @@
     fastfetch
     faugus-launcher
     gamescope
-    ghostty
+    obsidian
+    cmake
+    gcc
     git
     gnome.gvfs
     gvfs
     heroic
     hyfetch
     kitty
-    krita
     lm_sensors
     localsend
     osu-lazer-bin
     pavucontrol
-    pkgs.unstable.librewolf
-    pkgs.unstable.nwg-look
-    pkgs.unstable.vesktop
+    vesktop
     polkit_gnome
     protontricks
     protonup-qt
@@ -145,8 +146,9 @@
     win2xcur
     xcursor-themes
     xwayland-satellite
-    zsh
-    zsh-powerlevel10k
+    nwg-look
+    fzf
+    papirus-icon-theme
     (pkgs.wrapOBS {
       plugins = with pkgs.obs-studio-plugins; [
         wlrobs
